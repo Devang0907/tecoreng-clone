9c45c9e19ec5844c5d7842c5f370f4446ea0ad50
Devang <rakholiyadevang@gmail.com>
2025-02-14 17:12:49 +0530

    Make form section responsive

--- src/components/FormSection.jsx
+++ src/components/FormSection.jsx
@@ -29,19 +29,19 @@ const FormSection = () => {
       };
 
     return (
-        <section className="flex bg-[#03142e] px-15 min-h-screen bg-navy-900">
+        <section className="flex bg-[#03142e] pt-6 sm:pt-0 sm:px-15 min-h-screen bg-navy-900">
 
             {/* Left Section */}
 
-            <div className="w-1/2 p-6 ml-[35px] text-white">
-                <div className="space-y-6 mt-5">
+            <div className="invisible w-0 sm:visible sm:w-1/2 sm:p-6 sm:ml-[35px] text-white">
+                <div className="sm:space-y-6 sm:mt-5">
                     <h2 className="text-[32px] font-semibold">We Are Here To Help You</h2>
                     <h1 className="text-[49px] font-semibold">Let's Transform Your Vision Into Value.</h1>
 
-                    <div className="mt-12">
-                        <h3 className="text-[20px] font-semibold mb-4">Contact us</h3>
-                        <div className="space-y-2">
-                            <div className="flex items-center p-5 space-x-2">
+                    <div className="sm:mt-12">
+                        <h3 className="text-[20px] font-semibold sm:mb-4">Contact us</h3>
+                        <div className="sm:space-y-2">
+                            <div className="flex items-center sm:p-5 sm:space-x-2">
                                 <img src="/images/f-s.svg" alt="" />
                                 <span>biz.tecoreng</span>
                             </div>
@@ -69,10 +69,10 @@ const FormSection = () => {
             </div>
 
             {/* Right Section - Form */}
-            <div className="w-1/2 p-12">
-                <div className="bg-white rounded-[40px] p-8 w-[500px]">
-                    <h2 className="text-[55px] font-[500] mb-2">Get in <span className="bg-gradient-to-r from-[rgb(255,97,11)] via-[rgb(255,140,60)] to-[rgb(252,183,162)] bg-clip-text text-transparent">touch</span></h2>
-                    <p className="text-[16px] font-medium text-black mb-6 leading-[21px]">
+            <div className="w-full sm:w-1/2 p-[15px] sm:p-12">
+                <div className="bg-white rounded-[35px] sm:rounded-[40px] py-8 px-4 sm:p-8 w-[350px] sm:w-[500px]">
+                    <h2 className="text-[20px] sm:text-[55px] font-[500] mb-2">Get in <span className="bg-gradient-to-r from-[rgb(255,97,11)] via-[rgb(255,140,60)] to-[rgb(252,183,162)] bg-clip-text text-transparent">touch</span></h2>
+                    <p className="text-[10px] sm:text-[16px] font-medium text-black mb-6 leading-[21px]">
                         We are always ready to help. There are many ways to contact us. You may drop us a line, give us a call, send an email.
                     </p>
 
@@ -144,7 +144,7 @@ const FormSection = () => {
                             <ReCAPTCHA sitekey={import.meta.env.VITE_SITE_KEY} ref={recaptcha} />
                         </div>
 
-                        <button className="sm:w-full w-30 sm:mt-3 bg-gradient-to-r from-[#F47B55] to-[#FF3D00] text-white text-sm sm:text-3xl py-1 px-0.5 sm:py-3 sm:px-2 border-b-4 border-[#701d04] font-semibold rounded-xl transform transition-all duration-200 hover:-translate-y-1 hover:border-b-[5px] active:translate-y-0 active:border-b-4">
+                        <button className="w-full  sm:mt-3 bg-gradient-to-r from-[#F47B55] to-[#FF3D00] text-white text-sm sm:text-3xl py-1 px-0.5 sm:py-3 sm:px-2 border-b-4 border-[#701d04] font-semibold rounded-xl transform transition-all duration-200 hover:-translate-y-1 hover:border-b-[5px] active:translate-y-0 active:border-b-4">
                             SUBMIT
                         </button>
                     </form>
